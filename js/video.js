c52f998213600ce929179bdfcc06cdf255d2b42f
mayaposner <73802036+mayaposner@users.noreply.github.com>
2022-04-14 18:37:20 -0400

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -6,54 +6,60 @@ window.addEventListener("load", function() {
 	video.loop = false 
 });
 
+
 document.querySelector("#play").addEventListener("click", function() {
-	console.log("Play Video")
-	video.play()
+	console.log("Play Video");
+	video.play();
 	console.log('The current volume is ' + video.volume)
 	document.querySelector('#volume').innerHTML = video.volume * 100+'%'
 });
+
 
 document.querySelector("#pause").addEventListener("click", function() {
 	console.log("Pause Video")
 	video.pause()
 });
 
+
 document.querySelector("#slower").addEventListener("click", function() {
 	console.log("The current speed is " + video.playbackRate);
+	console.log('Slow Down')
 	video.playbackRate *= .95;
 	console.log('The new speed is ' + video.playbackRate)
 });
 
 document.querySelector("#faster").addEventListener("click", function() {
-	console.log("The current speed is " + video.playbackRate);
+	console.log("The current speed is " + video.playbackRate); 
+	console.log('Speed Up')
 	video.playbackRate /= .95;
 	console.log('The new speed is ' + video.playbackRate)
 });
 
 document.querySelector("#mute").addEventListener("click", function() {
-	console.log("The value of mute is " + video.muted); 
+	console.log("The value of mute is " + video.muted);
 	// have to check if currenly muted --> 
 	// if currenly muted, set muted to false & update text
 	// else set muted to true & update text
+	
+	if(video.muted == true){
+		video.muted = false;
+		document.querySelector("#volume").innerHTML = video.volume*100 + "%";
+		document.querySelector("#mute").innerHTML = "Mute"
 
-	if (mute == true){ 
-		video.muted = false; 
-		document.querySelector('#volume').innerHTML = video.volume*100 + '%'
-		document.querySelector('#mute').innerHTML = 'Mute'
 	}
-
-	else { 
-		video.muted = true; 
-		document.querySelector('#volume').innerHTML = '0%'
-		document.querySelector('#mute').innerHTML = 'Unmute'
+	else{
+		video.muted = true;
+		document.querySelector("#volume").innerHTML = "0%";
+		document.querySelector("#mute").innerHTML = "Unmute"
+	
 	}
+	
 });
 
 document.querySelector("#slider").addEventListener("click", function() {
-	console.log(this.value); 
-	video.volume = this.value/100; 
-	document.querySelector('volume').innerHTML = this.value + '%';
-	
+	console.log(this.value)
+	video.volume = this.value / 100
+	document.querySelector("#volume").innerHTML = this.value + "%";
 });
 
 document.querySelector("#skip").addEventListener("click", function() {
@@ -65,6 +71,7 @@ document.querySelector("#skip").addEventListener("click", function() {
 	}
 	
 });
+
 
 document.getElementById("vintage").addEventListener("click", function() {
 	video.classList.add('oldSchool')
